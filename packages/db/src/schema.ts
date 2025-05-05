ba80085cae0c00bda265defca6370f03b996dae7
skusez <imskuse@gmail.com>
2025-05-05 17:26:20 +1000

    init

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,5 +1,3 @@
-import { relations } from "drizzle-orm";
-import { integer } from "drizzle-orm/pg-core";
 import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -9,32 +7,5 @@ export const users = pgTable("users", {
   password: text("password").notNull(),
 });
 
-export const usersRelations = relations(users, ({ many }) => ({
-  projects: many(projects),
-}));
-
-export const projects = pgTable("projects", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-  updatedAt: timestamp("updated_at")
-    .notNull()
-    .defaultNow()
-    .$onUpdateFn(() => new Date()),
-  userId: integer("user_id")
-    .references(() => users.id, { onDelete: "cascade" })
-    .notNull(),
-});
-
-export const projectsRelations = relations(projects, ({ one }) => ({
-  user: one(users, {
-    fields: [projects.userId],
-    references: [users.id],
-  }),
-}));
-
-/* TODO: need to check trpc setup. trpc inference isn't working.. :'( */
-export type Project = typeof projects.$inferSelect;
-export type NewProject = typeof projects.$inferInsert;
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
